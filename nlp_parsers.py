db0048c94637b15a73596f6836eda72579cbf564
heidi-e <100874463+heidi-e@users.noreply.github.com>
2023-02-20 17:05:26 -0500

    functions for parsers type

--- nlp_parsers.py
+++ nlp_parsers.py
@@ -1,5 +1,6 @@
 
 import json
+import csv
 from collections import Counter
 
 
@@ -11,4 +12,13 @@ def json_parser(filename):
     wc = Counter(words)
     num = len(words)
     f.close()
+    return {'wordcount': wc, 'numwords': num}
+
+def csv_parser(filename):
+    with open(filename, 'w', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            for words in row:
+                wc = Counter(words)
+                num = len(words)
     return {'wordcount': wc, 'numwords': num}
