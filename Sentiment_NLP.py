a0fe6b3a55a7cedb44a361479f2e75c76077985f
heidi-e <100874463+heidi-e@users.noreply.github.com>
2023-02-27 15:21:28 -0500

    removed unneeded documentation

--- Sentiment_NLP.py
+++ Sentiment_NLP.py
@@ -1,16 +1,15 @@
 """
-Core framework class for NLP Comparative Analysis
+A reuseable core framework for NLP Comparative Text Analysis
+DS3500 HW3
+2/27/23
 """
 import pandas as pd
 import numpy as np
-from typing import List
-from collections import Counter, defaultdict
-import random
+from collections import defaultdict
 import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import pprint as pp
 from sankey_test import make_sankey
 from nltk.sentiment import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
@@ -20,65 +19,9 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('vader_lexicon')
 
-# change
-"""
-class Textastic:
-
-    def __init__(self):
-        # manage data about the different texts that
-        # we register with the framework
-        self.data = defaultdict(dict) # our data extracted from text files
-        self.viz = {}                 # name -> visualization function
-
-    @staticmethod
-    def _default_parser(filename):
-        results = {
-            'wordcount': Counter("to be or not to be".split()),
-            'numwords': random.randrange(10, 50)
-        }
-        return results
-
-    def _save_results(self, label, results):
-         Integrate parsing results into internal state
-        label: unique label for a text file that we parsed
-        results: the data extracted from the file as a dictionary attribute-->raw data
-
-        for k, v in results.items():
-            self.data[k][label] = v
-
-    def load_text(self, filename, label=None, parser=None):
-        Register a document with the framework 
-        if parser is None:  # do default parsing of standard .txt file
-            results = Textastic._default_parser(filename)
-        else:
-            results = parser(filename)
-
-        if label is None:
-            label = filename
-
-        # Save / integrate the data we extracted from the file
-        # into the internal state of the framework
-
-        self._save_results(label, results)
-
-    def load_visualization(self, name, vizfunc, *args, **kwargs):
-        self.viz[name] = (vizfunc, args, kwargs)
-
-    def visualize(self, name=None):
-        if name is None: # run all
-            for _, v in self.viz.items():
-                vizfunc, args, kwargs = v
-                vizfunc(self.data, *args, **kwargs)
-        else: # run only the named visualization
-            vizfunc, args, kwargs = self.viz[name]
-            vizfunc(self.data, *args, **kwargs)
-
-"""
-
-
 class SentimentNLP:
     """
-    class for analyzing text using natural language processing
+    class for analyzing text files using natural language processing
     """
 
     def __init__(self):
