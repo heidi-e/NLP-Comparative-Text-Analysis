0fb8cb510e451f04acf37863585fdc711e7d4a8d
conbobdoyle <doyle.c@northeastern.edu>
2023-02-27 23:14:22 -0500

    Final Commit - Conor

--- instantiate.py
+++ instantiate.py
@@ -6,22 +6,23 @@ from Sentiment_NLP import SentimentNLP
 
 
 
+
 object = SentimentNLP()
 
-object.load_text('Text_Files/michael-jackson.txt')
-object.load_text('Text_Files/lorde.txt')
-object.load_text('Text_Files/nirvana.txt')
-object.load_text('Text_Files/rihanna.txt')
-object.load_text('Text_Files/amy-winehouse.txt')
+object.load_text('michael-jackson.txt')
+object.load_text('lorde.txt')
+object.load_text('nirvana.txt')
+object.load_text('rihanna.txt')
+object.load_text('amy-winehouse.txt')
 
 # create sankey diagram
-#object.wordcount_sankey()
+object.wordcount_sankey()
 
 # create word cloud visualization
 object.second_viz()
 
 # create multiple bar plot
-#object.third_viz()
+object.third_viz()
 
 
 
